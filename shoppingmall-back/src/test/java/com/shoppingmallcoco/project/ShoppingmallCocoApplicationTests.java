c64e3483f9e6e7a78db573bbf6aa3b8c8036d5ec
tae-co <dcnfsjfzp2@privaterelay.appleid.com>
2025-10-24 15:58:07 +0900

    Add full ShoppingMall project including Front and shoppingmall-back

---
+++ shoppingmall-back/src/test/java/com/shoppingmallcoco/project/ShoppingmallCocoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingmallcoco.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallCocoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
