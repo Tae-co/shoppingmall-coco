f0e5275742e6acf29cb80f81073d4a3e190aa1c9
tae-co <dcnfsjfzp2@privaterelay.appleid.com>
2025-11-01 18:29:43 +0900

    refactor:create folder

--- shoppingmall-back/src/main/java/com/shoppingmallcoco/project/ShoppingmallCocoApplication.java
+++ shoppingmall-back/src/main/java/com/shoppingmallcoco/project/ShoppingmallCocoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShoppingmallCocoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShoppingmallCocoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingmallCocoApplication.class, args);
+    }
 
 }
