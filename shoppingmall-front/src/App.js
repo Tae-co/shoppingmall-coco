1e7e0835b2226cd6a31ec6a0ea36687583819b04
Choo HyunMin <chu072409@gmail.com>
2025-11-04 12:37:00 +0900

    feat:login 페이지 추가

--- shoppingmall-front/src/App.js
+++ shoppingmall-front/src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Login from './pages/Login';
 
 function App() {
+  const location = useLocation();
+  const hideHeaderFooter = ['/login'].includes(location.pathname);
+
   return (
     <div className="App">
-      <Header />
-      <Footer />
+      {!hideHeaderFooter && <Header />}
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      {!hideHeaderFooter && <Footer />}
     </div>
   );
 }
