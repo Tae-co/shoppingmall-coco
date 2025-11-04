1a5616c5d0c2541261d220c58bd96b713c882a22
Choo HyunMin <chu072409@gmail.com>
2025-11-04 13:07:21 +0900

    feat:아이디/비밀번호 찾기 폼

--- shoppingmall-front/src/App.js
+++ shoppingmall-front/src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Login from './pages/Login';
+import SignupTerms from './pages/SignupTerms';
+import SignupInfo from './pages/SignupInfo';
+import FindAccount from './pages/FindAccount';
 
 function App() {
+  const location = useLocation();
+  const hideHeaderFooter = ['/login', '/signup/terms', '/signup/info', '/find-account'].includes(location.pathname);
+
   return (
     <div className="App">
-      <Header />
-      <Footer />
+      {!hideHeaderFooter && <Header />}
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup/terms" element={<SignupTerms />} />
+        <Route path="/signup/info" element={<SignupInfo />} />
+        <Route path="/find-account" element={<FindAccount />} />
+      </Routes>
+      {!hideHeaderFooter && <Footer />}
     </div>
   );
 }
