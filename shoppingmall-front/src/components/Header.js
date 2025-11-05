8f2f50c627d653973defe60945ac6e5d827e6364
LeeJunBeom159 <beom0963@naver.com>
2025-11-05 11:15:43 +0900

    import 삭제

--- shoppingmall-front/src/components/Header.js
+++ shoppingmall-front/src/components/Header.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import {NavLink, Link, Routes, Route} from 'react-router-dom';
 import Logo from '../images/logo.png';
 import '../css/Header.css';
-import Cart from "../pages/Cart";
+
 
 
 const Header = () => {
