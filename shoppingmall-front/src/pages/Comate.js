89aed7547fe974a74400e0d93ceaf25df587eed4
CurieLee-alt <curiepongs2@naver.com>
2025-11-12 01:55:16 +0900

    feat:Comate 프로필 카드 구현

--- shoppingmall-front/src/pages/Comate.js
+++ shoppingmall-front/src/pages/Comate.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import '../css/Comate.css';
+
+import ComateFullProfile from './ComateFullProfile';
+import ComateContent from './ComateContent';
 
 const Comate = () => {
+    const [activeTab, setActiveTab] = useState('review');
+    const handleTabChange = (tabName) => {
+        setActiveTab(tabName);
+    };
+
+    // co-mate 데이터 (임시)
+    const profile = {nickname: "뷰티소연", skinTypes: ["건성", "민감성"], likes: 152, followers: 1280, following: 340, isFollowing: false,
+        onFollowClick: () => console.log("팔로우 클릭"),
+        onClick: () => console.log("프로필 클릭"),
+        onTabClick: handleTabChange
+    };
+
+    const reviewList = [
+        "리뷰 1: 이 제품 진짜 좋아요!",
+        "리뷰 2: 촉촉하고 산뜻합니다.",
+        "리뷰 3: 재구매 의사 있음!"
+    ];
+    const likeList = [
+        "좋아요 게시물 1",
+        "좋아요 게시물 2",
+        "좋아요 게시물 3"
+    ];
+    const followerList = [
+        "팔로워 유저A",
+        "팔로워 유저B",
+        "팔로워 유저C"
+    ];
+    const followingList = [
+        "팔로잉 유저X",
+        "팔로잉 유저Y",
+        "팔로잉 유저Z"
+    ];
+
     return (
-        <div>
-            comate content
+        <div className="comate_wrapper">
+            <ComateFullProfile {... profile} />
+            <ComateContent 
+                activeTab={activeTab}
+                reviewList={reviewList}
+                likesList={likeList}
+                followersList={followerList}
+                followingList={followingList}
+            />
         </div>
     );   
 }
---
+++ shoppingmall-front/src/pages/ComateFullProfile.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+import '../css/ComateProfile.css';
+import sampleImg_profile from '../images/sampleImg_profile.png'; // 임시 프로필 이미지
+
+const ComateFullProfile = ({nickname, skinTypes, likes, followers, following, 
+                            onFollowClick, onClick, isFollowing, onTabClick}) => {
+    return (
+        <div className="comate_card_wrapper" onClick={onClick}>
+            <div className="comate_card full">
+            <div className="profile_section full">
+                <img src={sampleImg_profile} alt="user_profile" className="profile_img full" />
+                <div className="nickname full">{nickname}</div>
+                <div className="skin_types full">
+                    {skinTypes?.map((type, index) => (
+                        <span key={index}>{type}</span>
+                    ))}
+                </div>
+            </div>
+            <div className="stats_section full">
+                <div className="stat_item full"
+                onClick={(e) => { e.stopPropagation(); onTabClick('like'); }}>
+                    <div className="stat_value full">{likes}</div>
+                    <div className="stat_label full">좋아요</div>
+                </div>
+                <div className="stat_item full"
+                onClick={(e) => { e.stopPropagation(); onTabClick('follower'); }}>
+                    <div className="stat_value full">{followers}</div>
+                    <div className="stat_label full">팔로워</div>
+                </div>
+                <div className="stat_item full"
+                onClick={(e) => { e.stopPropagation(); onTabClick('following'); }}>
+                    <div className="stat_value full">{following}</div>
+                    <div className="stat_label full">팔로잉</div>
+                </div>
+            </div>
+            </div>
+            <button
+                className={`follow_btn full ${isFollowing ? "active" : ""}`}
+                onClick={(e) => {
+                    e.stopPropagation();
+                    onFollowClick();
+                }}
+            >
+                {isFollowing ? "팔로잉" : "팔로우"}
+            </button>
+        </div>
+    );
+};
+
+export default ComateFullProfile;
