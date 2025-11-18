d8be87bfec74db94bc496d45ae3bda7d6478d154
CurieLee-alt <curiepongs2@naver.com>
2025-11-18 10:41:57 +0900

    feat:프로필 조회 및 사용자 구분

--- shoppingmall-back/src/main/java/com/shoppingmallcoco/project/dto/comate/ProfileDTO.java
+++ shoppingmall-back/src/main/java/com/shoppingmallcoco/project/dto/comate/ProfileDTO.java
@@ -1,5 +1,17 @@
 package com.shoppingmallcoco.project.dto.comate;
 
-public class ProfileDTO {
+import lombok.*;
 
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProfileDTO {
+	
+	private Long memNo;
+	private String memName;
+	private String memNickname;
+	
+	private boolean isMyProfile;
 }
--- shoppingmall-back/src/main/java/com/shoppingmallcoco/project/service/comate/ComateService.java
+++ shoppingmall-back/src/main/java/com/shoppingmallcoco/project/service/comate/ComateService.java
@@ -1,5 +1,31 @@
 package com.shoppingmallcoco.project.service.comate;
 
+import org.springframework.stereotype.Service;
+
+import com.shoppingmallcoco.project.dto.comate.ProfileDTO;
+import com.shoppingmallcoco.project.entity.Member;
+import com.shoppingmallcoco.project.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class ComateService {
 
-}
+	private final MemberRepository memberRepository;
+	
+	public ProfileDTO getProfile(Long profileMemNo, Long currentMemNo) {
+		Member member = memberRepository.findById(profileMemNo)
+				.orElseThrow(() -> new RuntimeException("회원이 존재하지 않습니다."));
+		
+		// 내 프로필인지 체크
+		boolean isMine = profileMemNo.equals(currentMemNo);
+		
+		return ProfileDTO.builder()
+				.memNo(member.getMemNo())
+				.memName(member.getMemName())
+				.memNickname(member.getMemNickname())
+				.isMyProfile(isMine)
+				.build();
+	}
+ }
