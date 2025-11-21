b4809b8f196f214926295527da9c91f9f25a0706
CurieLee-alt <curiepongs2@naver.com>
2025-11-21 11:34:03 +0900

    chore:CO-MATE 테스트용 더미데이터 삭제

--- shoppingmall-front/src/pages/Home_Comate.js
+++ shoppingmall-front/src/pages/Home_Comate.js
@@ -52,19 +52,6 @@ function Home_Comate() {
         loadComates();
     }, []);
 
-    /* 
-    co-mate 데이터 (임시)
-    const comates = [
-        {id: 1, nickname: "홍길동", skinTypes: ["건성", "민감성"], followers: 120, reviews: 15},
-        {id: 2, nickname: "뷰티러버", skinTypes: ["지성", "가을웜"], followers: 300, reviews: 45},
-        {id: 3, nickname: "포테이토칩", skinTypes: ["복합성", "여름뮤트"], followers: 80, reviews: 12},
-        {id: 4, nickname: "익명", skinTypes: ["중성"], followers: 200, reviews: 30},
-        {id: 5, nickname: "이몽룡", skinTypes: ["봄웜"], followers: 800, reviews: 300},
-        {id: 6, nickname: "코스메틱러버", skinTypes: ["지성", "겨울쿨"], followers: 150, reviews: 25},
-        {id: 7, nickname: "뷰티마스터", skinTypes: ["복합성", "가을웜"], followers: 500, reviews: 60}
-    ];
-    */
-
     // 프로필 클릭-> 상세 프로필 이동
     const handleCardClick = (memNo) => {
         navigate(`/comate/user/${memNo}/review`);
